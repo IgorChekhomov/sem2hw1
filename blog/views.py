9ee47af9083fe438ae7659b8089811dfbf1be1a5
IgorChekhomov <123004384+IgorChekhomov@users.noreply.github.com>
2023-04-07 13:31:02 +0500

    Add files via upload

---
+++ blog/views.py
@@ -0,0 +1,17 @@
+from django.views import generic
+from .models import Post
+from .serializers import PostSerializer
+from rest_framework import viewsets
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
